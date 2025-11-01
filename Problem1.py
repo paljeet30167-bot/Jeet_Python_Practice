dbb74d144580b91fa2a15dc3fdb01d42831258a3
Jitendra <paljeet30167@gmail.com>
2025-11-01 11:20:13 +0530

    Program _ The number is Possitive or Negative
     Write a Python  to Find the number is Possitive or Negative

---
+++ Problem1.py
@@ -0,0 +1,10 @@
+# Write a Python Program to Find the number is Possitive or Negative
+
+num = input ("Enter a number:")
+number = int(num)
+if(number > 0):
+    print("number is positive")
+elif(number < 0 ):
+    print("Number is Negative")
+else:
+    print("Number is zero")
