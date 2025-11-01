09455937814aa24c0944899ee9df1530f1d2f046
Jitendra <paljeet30167@gmail.com>
2025-11-01 11:23:01 +0530

    external module perform operation of your internet
    # Install an external module and use it to perform an operation of your internet

---
+++ Problem2.py
@@ -0,0 +1,6 @@
+# Install an external module and use it to perform an operation of your internet
+import pyttsx3
+engine = pyttsx3.init()
+
+engine.say(" Hello Jitendra , How i can help you?")
+engine.runAndWait()
