b7fcf2d4324ab2ce7b6c5bb28df951aab8d170fc
Jitendra <paljeet30167@gmail.com>
2025-11-04 11:20:08 +0530

    check number is grater then 10 or divisible by 2
    #wite a Program to take a input from the user as a number to check number is grater then 10 or divisible to the 2 then print the number is decimal

---
+++ Problem7.py
@@ -0,0 +1,13 @@
+#wite a progrma to take a input from the user as a number to check nunber is grater then 10 or divisibke to the 2 then print the number is decimal 
+num = input("Enter the number:")
+number = int(num)
+if(number >= 10 and number % 2 == 0):
+    print("True")
+else:
+    print("false")
+
+
+# if(0.00000001):
+#     print("i m Deepak")
+# else:
+#     print(" i m Jitnedra")
