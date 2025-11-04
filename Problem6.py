c8cc5384f29f8af4db0d45230f48157e4cade7e9
Jitendra <paljeet30167@gmail.com>
2025-11-04 11:17:29 +0530

    Program to Find the person is Eligible for Vote
    # Write a Python Program to Find the person is Eligible for Vote

---
+++ Problem6.py
@@ -0,0 +1,9 @@
+# Write a Python Program to Find the person is Eligible for Vote
+
+
+age= input("Enter age:")
+number = int(age)
+if( number >= 18 ):
+    print("eligible for Vote")
+else:
+    print("Not eligible")
