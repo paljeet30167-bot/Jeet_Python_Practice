71f97f0a6f78a6b9cde428ec22df6b59f3a0dcbb
Jitendra <paljeet30167@gmail.com>
2025-11-04 11:16:05 +0530

    Program to Find the number is Even Or Odd
    # Write a Python Program to Find the number is Even Or Odd

---
+++ Problem5.py
@@ -0,0 +1,8 @@
+# Write a Python Program to Find the number is Even Or Odd
+
+num = input("Enter the number:")
+number= int(num)
+if(number % 2 ==0):
+    print("Number is Even")
+else:
+    print("Number is Odd")
